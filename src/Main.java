f39be65ea21ba8eb309488307b478756475d2347
tahmidchowdhury21 <tahmidchowdhury127@gmail.com>
2022-05-04 10:29:05 -0400

    refactored the style of the code

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {		
-		
+	public static void main(String[] args) {
+
 		// Instantiating the class variable
 		PasswordGenerator passwordGenerator = new PasswordGenerator();
-		
+
 		// Getting the userInputs
 		Scanner userInput = new Scanner(System.in);
 		System.out.print("\nEnter the length of the password: ");
-		
+
 		int length = userInput.nextInt();
 		
 		System.out.println();
-		
+
 		// generated password
 		String generatedPassword = passwordGenerator.generateRandomPassword(length);
 		System.out.println(generatedPassword);
+		
+		userInput.close();
 
 	}
 
